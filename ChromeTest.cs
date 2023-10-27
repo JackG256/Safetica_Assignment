b12900a820a7f11d0d2073d30535c73b65d9667b
Ladislav Kollár <l_kollar@utb.cz>
2023-10-27 19:23:05 +0200

    Split logic into separate class files

---
+++ ChromeTest.cs
@@ -0,0 +1,29 @@
+namespace Safetica_Assignment;
+
+[TestFixture]
+    public class ChromeTests : CommonTest
+    {
+        [Test]
+        public void ChromeTest()
+        {
+                Assert.Pass();
+        }
+
+        [Test]
+        public void ChromeTest2()
+        {
+                Assert.Pass();
+        }
+
+        [Test]
+        public void ChromeTest3()
+        {
+                Assert.Pass();
+        }
+
+        [Test]
+        public void ChromeTest4()
+        {
+                Assert.Pass();
+        }
+    }
