e93756960f5a0183ce9d46f1d0d7520f57b29271
Ladislav Kollár <l_kollar@utb.cz>
2023-10-27 22:29:11 +0200

    Optimized method calls

--- TestLogicChrome.cs
+++ TestLogicChrome.cs
@@ -19,14 +19,14 @@ public class TeamsPage
         _driver.Navigate().GoToUrl(Constants.teamsUrl);
         _actionLogger.Log($"Loaded website: [{Constants.teamsUrl}]");
 
-        IWebElement loginEmailField = AssistFunc.WaitForElement(_driver, By.TagName("input"), 5);
+        IWebElement? loginEmailField = AssistFunc.WaitForElement(_driver, By.TagName("input"), 5);
         Assert.IsNotNull(loginEmailField);
     }
 
     public void LogInTeams()
     {
         // Search for email input field, then input email
-        IWebElement loginEmailField = AssistFunc.WaitForElement(_driver, By.Id("i0116"), 5);
+        IWebElement? loginEmailField = AssistFunc.WaitForElement(_driver, By.Id("i0116"), 5);
         Assert.IsNotNull(loginEmailField); 
         _actionLogger.Log("Email input element found");
 
@@ -40,10 +40,10 @@ public class TeamsPage
         _actionLogger.Log($"User pressed \"submit\" button");
 
         // Explicit wait to avoid stale elements because of animations
-        Thread.Sleep(1000);
+        Thread.Sleep(2000);
 
         // Check the page updated by looking for password input field
-        IWebElement loginPassField = AssistFunc.WaitForElement(_driver, By.Id("i0118"), 5);
+        IWebElement? loginPassField = AssistFunc.WaitForElement(_driver, By.Id("i0118"), 5);
         Assert.IsNotNull(loginPassField);
         _actionLogger.Log("password input element found");
 
@@ -54,32 +54,37 @@ public class TeamsPage
         _actionLogger.Log($"User input: \"{Constants.pass}\"");
 
         // Submit password
-        submitButton = _driver.FindElement(By.TagName("input[type='submit']"));
+        submitButton = _driver.FindElement(By.Id("idSIButton9"));
         submitButton.Click();
         _actionLogger.Log($"User pressed \"submit\" button");
     }
 
     public void FilterPopUps()
     {
-        IWebElement staySignedPopup = AssistFunc.WaitForElement(_driver, By.LinkText("Zůstat přihlášen(a)?"), 5);
-        if (staySignedPopup == null){
-            staySignedPopup = AssistFunc.WaitForElement(_driver, By.LinkText("Stay signed in?"), 5);
-        }
+        // Hard sleep to avoid DOM change during animations
+        Thread.Sleep(3000);
+
+        IWebElement? staySignedPopup = AssistFunc.WaitForElement(_driver, By.XPath("//*[@id=\"lightbox\"]/div[3]/div/div[2]/div/div[1]"), 5);
 
         if (staySignedPopup != null){
-            IWebElement noButton = _driver.FindElement(By.TagName("button"));
+            IWebElement noButton = _driver.FindElement(By.XPath("//*[@id=\"idBtn_Back\"]"));
             noButton.Click();
+            _actionLogger.Log("Found a \"Stay Signed In\" pop-up, clearing!");
+        }else{
+            _actionLogger.Log("Didn't find a \"Stay Signed In\" pop-up...");
         }
 
-
-        IWebElement getAppPopup = AssistFunc.WaitForElement(_driver, By.LinkText("Stáhněte si aplikaci Teams pro plochu a zajistěte si lepší připojení."), 5);
-        if (getAppPopup == null){
-            getAppPopup = AssistFunc.WaitForElement(_driver, By.LinkText("Download the Teams desktop app and stay better connected."), 5);
-        }
+        // Hard sleep to avoid DOM change during animations
+        Thread.Sleep(7000);
+        
+        IWebElement? getAppPopup = AssistFunc.WaitForElement(_driver, By.ClassName("download-text"), 5);
         
         if(getAppPopup != null){
             IWebElement useWebApp = _driver.FindElement(By.ClassName("use-app-lnk"));
             useWebApp.Click();
+            _actionLogger.Log("Found a \"Use App\" pop-up, clearing!");
+        }else{
+             _actionLogger.Log("Didn't find a \"Use App\" pop-up...");
         }
     }
 }
