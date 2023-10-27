35040ab318884b5f736538962aeda1903167bf3f
Ladislav Kollár <l_kollar@utb.cz>
2023-10-27 21:49:05 +0200

    Added trailing newline

--- Logger.cs
+++ Logger.cs
@@ -2,27 +2,27 @@ namespace Safetica_Assignment;
 
 public class Logger
 {
-    // Initiate path variable  to file
-    private readonly string _logFilePath;
-    
-    public Logger(string logDirectory, string logFileName)
-    {
-        var baseDir = AppDomain.CurrentDomain.BaseDirectory; 
-        var projectRoot = Path.GetFullPath(Path.Combine(baseDir, "..", "..", "..")); 
-        var fullLogDirectory = Path.Combine(projectRoot, logDirectory);
+	// Initiate path variable  to file
+	private readonly string _logFilePath;
+	
+	public Logger(string logDirectory, string logFileName)
+	{
+		var baseDir = AppDomain.CurrentDomain.BaseDirectory; 
+		var projectRoot = Path.GetFullPath(Path.Combine(baseDir, "..", "..", "..")); 
+		var fullLogDirectory = Path.Combine(projectRoot, logDirectory);
 
-        if (!Directory.Exists(fullLogDirectory))
-        {
-            Directory.CreateDirectory(fullLogDirectory);
-        }
+		if (!Directory.Exists(fullLogDirectory))
+		{
+			Directory.CreateDirectory(fullLogDirectory);
+		}
 
-        _logFilePath = Path.Combine(fullLogDirectory, logFileName);
-    }
+		_logFilePath = Path.Combine(fullLogDirectory, logFileName);
+	}
 
-    // Logging function
-    public void Log(string message)
-    {
-        var logMessage = $"[{DateTime.Now:dd-MM-yyyy; hh:mm:ss}] - {message}";
-        File.AppendAllText(_logFilePath, logMessage + Environment.NewLine);
-    }
+	// Logging function
+	public void Log(string message)
+	{
+		var logMessage = $"[{DateTime.Now:dd-MM-yyyy; hh:mm:ss}] - {message}";
+		File.AppendAllText(_logFilePath, logMessage + Environment.NewLine);
+	}
 }
