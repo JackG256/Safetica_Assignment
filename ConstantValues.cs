83de2f5bf65a37179b776fa20fc9060cc93ae4cb
Ladislav Kollár <l_kollar@utb.cz>
2023-10-27 21:48:46 +0200

    Implemented a file to store global constants

---
+++ ConstantValues.cs
@@ -0,0 +1,13 @@
+namespace Safetica_Assignment;
+
+public static class Constants
+{
+    public const string teamsUrl = "https://teams.microsoft.com/";
+    public const string login = "dlp.automation3@safeticadlptesting.onmicrosoft.com";
+    public const string pass = "Password.dlp";
+
+
+
+
+
+}
